aa11078f18e0c9a63d2f7811d85005c1b2d749b7
aryanninawe811 <aryanninawe007@gmail.com>
2026-09-18 12:13:26 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>a.addEventListener('click',e=>{const t=document.querySelector(a.getAttribute('href'));if(t){e.preventDefault();t.scrollIntoView({behavior:'smooth'})}}));
+document.querySelectorAll('nav a').forEach(link=>{link.addEventListener('click',()=>{document.querySelectorAll('nav a').forEach(a=>a.removeAttribute('aria-current'));link.setAttribute('aria-current','page')})});
